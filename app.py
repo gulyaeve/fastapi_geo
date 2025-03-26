b619eb710860d6f4458078606027fa52b39e92a0
gulyaeve <gulyaeve@icloud.com>
2025-03-26 16:14:50 +0300

    Added cache

--- app.py
+++ app.py
@@ -1,12 +1,37 @@
 from fastapi import FastAPI
 from city.city import router as geo_router
+from fastapi_redis_cache import FastApiRedisCache, cache
+from settings import settings
+from time import sleep
 
 
 app = FastAPI()
 
 app.include_router(geo_router)
 
+
+@app.on_event("startup")
+def start():
+    redis_cache = FastApiRedisCache()
+    redis_cache.init(
+        host_url=settings.redis_url
+    )
+
 @app.get("/")
 async def get_root():
     return {"Hello": "World"}
 
+
+# Не будет кэшировано
+@app.get("/users")
+async def get_users():
+    sleep(1)
+    return {"Users": {"name": "vasya"}}
+
+
+# Будет кэшировано
+@app.get("/users2")
+@cache(expire=60)
+async def get_users_2():
+    sleep(1)
+    return {"Users2": {"name": "vasya"}}
---
+++ settings.py
@@ -0,0 +1,16 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    REDIS_HOST: str
+    REDIS_PORT: int
+
+    @property
+    def redis_url(self):
+        return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}"
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
