9a8155fc607824d4e32515bc14b0067d6c1052c5
gulyaeve <gulyaeve@icloud.com>
2025-03-27 07:59:41 +0300

    Added redis channel

--- utils/image.py
+++ utils/image.py
@@ -1,5 +1,6 @@
 from PIL import Image
 from pathlib import Path
+from config import cache
 
 
 def resize(file_name: str):
@@ -8,11 +9,21 @@ def resize(file_name: str):
 
     image_resized_1000_500 = image.resize((1000, 500))
     image_resized_200_100 = image.resize((200, 100))
-    
+
     image_resized_1000_500.save(f"temp/{file_path.name}_1000_500.{file_path.suffix}")
     image_resized_200_100.save(f"temp/{file_path.name}_200_100.{file_path.suffix}")
 
 
+def image_subscribe():
+    image_channel = cache.pubsub()
+    image_channel.subscribe("images")
+    print("Подписан на канал images")
+    for message in image_channel.listen():
+        if message['type'] == "message":
+            resize(message['data'].decode())
+            print(f"{message['data']} resized")
+
+
 
 if __name__ == "__main__":
-    ...
+    image_subscribe()
